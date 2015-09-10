d419a30bdd277645d0c3eb674a5986543be591dd
Susan Tyler <st421@users.noreply.github.com>
2015-09-10 18:47:09 -0400

    Update event.php
    Updates for convenience methods for other parts of the plugin.

--- src/event.php
+++ src/event.php
@@ -13,13 +13,11 @@ class EventElement {
     $value = $val;
   } 
 
-  public function header() {
+  public function table_header_title_string() {
     return "<th>" . $name . "</th>";
   }
   
-  
-  
-  public function table_entry() {
+  public function table_row_init() {
     return name_for_table() . " " . $type . "(" . $size . ")";
   }
   
@@ -75,7 +73,7 @@ class Event {
   public $brief = new Brief();
   public $description = new Description();
   
-  function set_fields($tit, $dat, $tim, $loc, $bri, $des) { 
+  public function set_fields($tit, $dat, $tim, $loc, $bri, $des) { 
     $title->value = $tit;
     $date->value = $dat;
     $time->value = $tim;
@@ -84,45 +82,76 @@ class Event {
     $description->value = $des;
   }
   
-  function fields_as_array() {
+  public function fields_as_array() {
     return array($title, $date, $time, $location, $brief, $description);
   }
   
-  function header() {
+  public function table_header_string() {
     $header = "<thead>";
     foreach (fields_as_array() as $event_element) {
-      $header .= $event_element->header();
+      $header .= $event_element->table_header_title_string();
     }
     $header .= "</thead>";
     return $header;
   }
   
-  function table_row_query($chopped = 1) {
+  public function table_row_string() {
+    $header = "<tr>";
+    foreach (fields_as_array() as $event_element) {
+      $header .= "<td>" . $event_element->value . "</td>";
+    }
+    $header .= "</tr>";
+    return $header;
+  }
+  
+  public function get_page_link_start($page_id) {
+    return '<a href="/?page_id=' . $page_id . '&title=' . $title . '&date=' . $date . '">';
+  }
+  
+  public function list_element_string($page_id) {
+    $list_str = '<li>' . get_page_link_start();
+    $list_str .= '<h3>' . $title . '</h3>';
+    $list_str .= '<h3 class="date">' . tec_format_date($event->date, '-', '.') . '</h3>';
+    $list_str .= '</a>' . $event->brief . '</li></br>';
+    return $list_str;
+  }
+  
+  public function table_row_query($chopped = 1) {
     foreach ($event->fields_as_array() as $event_element) {
       $sql_query .= $event_element->name_for_table() . ",";
     }
     return do_chop($chopped, $sql_query);
   }
   
-  function table_row_values($chopped = 1) {
+  public function table_row_values($chopped = 1) {
     foreach ($event->fields_as_array() as $event_element) {
       $sql_query .= "'" . $event_element->value . "',";
     }
     return do_chop($chopped, $sql_query);
   }
   
-  function table_row_update($chopped = 1) {
+  public function table_row_update($chopped = 1) {
     foreach ($event->fields_as_array() as $event_element) {
       $sql_query .= $event_element->title . "='" . $event_element->value . "',";
     }
     return do_chop($chopped, $sql_query);
   }
   
-    function do_chop($chopped, $str) {
+  function do_chop($chopped, $str) {
     if($chopped) {
       return chop($str,",");
     }
     return $str;
   }
-}
+  
+  function tec_format_date($date, $old, $new) {
+  	$pieces = explode($old,$date);
+  	if($new == '-') {
+  		$new_date = $pieces[2] . $new . $pieces[0] . $new . $pieces[1];
+  	} else {
+  		$new_date = $pieces[1] . $new . $pieces[2] . $new . $pieces[0];
+  	}
+  	return $new_date;
+    }
+  }
 ?>
